f9e08db7d5c17f3f88a9a2f454540a984efecb29
Apoorv888 <apoorvap795@gmail.com>
2025-11-04 14:55:50 +0530

    cmd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+    chunkSizeWarningLimit: 700,
+  },
 })
